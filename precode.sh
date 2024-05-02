c0aa3c9a17f7bd050ec352079c9889dce84b995f
rsa <nicedayone7@gmail.com>
2024-05-02 16:10:35 +0300

    Add sort

--- precode.sh
+++ precode.sh
@@ -18,7 +18,7 @@ touch dir2/empty
 # создаём файл task/dir2/hello.sh с таким содержанием:
 # #!/bin/bash
 # echo "$1, привет!" 
-echo -e '#!/bin/bash\n echo "$1, привет!"' > hello.sh
+echo -e '#!/bin/bash\n echo "$1, привет!"' >> dir2/hello.sh
 # устанавливаем для task/dir2/hello.sh права rwxrw-r--
 chmod 764 dir2/hello.sh
 
@@ -35,17 +35,16 @@ find . -type f -name "*.txt" > dir1/summary.txt
 # дописываем в task/dir1/summary.txt содержимое task/dir2/list.txt
 cat dir2/list.txt >> dir1/summary.txt
 # определяем переменную окружения NAME со значением "Всем студентам"
-export NAME="Всем студентам"
+export NAME='Всем студентам'
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
-dir2/hello.sh $NAME >> dir1/summary.txt
+dir2/hello.sh "$NAME" >> dir1/summary.txt
 # перемещаем с переименованием task/dir1/summary.txt в task/Практическое задание
 mv dir1/summary.txt "Практическое задание"
 # выводим на консоль содержимое файла task/Практическое задание
-cat "Практическое задание"
 # ищем в файле "Практическое задание" строки, которые содержат слово "dir"
 # и затем отсортировываем их
-cat "Практическое задание" | grep "dir"
+cat "Практическое задание" | grep "dir" | sort
 # меняем текущую директорию на родительскую для task
 cd ..
 # удаляем директорию task со всем содержимым
