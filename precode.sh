3236e5df1de1e0609cf87b5c267645dd2fbf27ad
rsa <nicedayone7@gmail.com>
2024-05-02 15:42:05 +0300

    Removed commands touch and environment variable is assigned

--- precode.sh
+++ precode.sh
@@ -16,7 +16,6 @@ cd task
 touch dir2/empty
 
 # создаём файл task/dir2/hello.sh с таким содержанием:
-touch dir2/hello.sh
 # #!/bin/bash
 # echo "$1, привет!" 
 echo -e '#!/bin/bash\n echo "$1, привет!"' > hello.sh
@@ -24,7 +23,6 @@ echo -e '#!/bin/bash\n echo "$1, привет!"' > hello.sh
 chmod 764 dir2/hello.sh
 
 # сохраняем список файлов task/dir2 в task/dir2/list.txt
-touch dir2/list.txt
 ls dir2 > dir2/list.txt
 
 # копируем содержимое каталога task/dir2 в каталог task/dir3/dir4
@@ -32,13 +30,12 @@ cp -r dir2 dir3/dir4
 
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
-touch dir1/summary.txt
 find . -type f -name "*.txt" > dir1/summary.txt
 
 # дописываем в task/dir1/summary.txt содержимое task/dir2/list.txt
 cat dir2/list.txt >> dir1/summary.txt
 # определяем переменную окружения NAME со значением "Всем студентам"
-NAME="Всем студентам"
+export NAME="Всем студентам"
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
 dir2/hello.sh $NAME >> dir1/summary.txt
